735a46229197957a027491799188ca4ac17c93a4
sujitmemane-auragold <sujit@auragold.in>
2026-07-06 01:12:25 +0530

    feat: add banner retrieval and storage endpoints

--- class-2/src/index.js
+++ class-2/src/index.js
@@ -4,11 +4,26 @@ import mongoose from "mongoose"
 
 const app = express()   
 
+app.use(express.json())
+
 
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379")
 
 
 
+const BANNER_KEY = "banner"
+
+app.get("/banner", async (req, res) => {
+    const banner = await redis.get(BANNER_KEY)
+    return res.json({ banner })
+})
+
+app.post("/banner", async (req, res) => {
+    console.log(req.body,"REQ_BODU")
+    const { banner } = req.body
+    await redis.set(BANNER_KEY, banner)
+    return res.json({ banner })
+})
 
 app.get("/", async (req, res) => {    
     const reply = await redis.ping()
